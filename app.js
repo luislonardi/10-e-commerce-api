90eb76f1e877599ab4df4a1cb14348ec12b044e9
luislonardi <luislonardi@gmail.com>
2022-05-26 18:33:59 -0300

    primer commit

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+require('dotenv').config();
+require('express-async-errors')
+
+const express= require('express')
+const app= express()
+//database
+
+const connectDB=require('./db/connect')
+
+
+
 console.log('E-Commerce API');
+ 
+const port = process.env.PORT  || 5000;
+
+const start= async ()=>{
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port,()=> console.log(`Server listening on port: ${port}...`));
+    }catch(error) {
+        console.log(error)
+    }
+    
+}
+start()
