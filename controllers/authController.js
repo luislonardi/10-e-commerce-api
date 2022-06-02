88207d92a42a074742f92871aa9283069e2aec6d
luislonardi <luislonardi@gmail.com>
2022-06-02 17:30:34 -0300

    2 de junio

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,6 +18,8 @@ const register= async (req,res)=>{
     const tokenUser={name:user.name,userId:user._id,role:user.role}
     attachCookiesToResponse({res,user:tokenUser})
     res.status(StatusCodes.CREATED).json({user:tokenUser});
+    console.log(req.body)
+    console.log({user:tokenUser})
   }
 
 const login= async (req,res)=>{
